92f3be07b051d8e042d0f8ccd1d3da4977cf1e91
DEVair Martins <devair.martins@gmail.com>
2023-03-24 16:05:52 -0300

    banco

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express  from 'express';
+import UsuarioController from './controllers/usuarioController.js';
 
 const app = express();
 
 app.use(express.json());
+
+UsuarioController(app)
 
 export default app;
---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,21 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+function UsuarioController(app){
+
+  app.get('/usuario', exibir)
+  function exibir (req, res){
+    (async () =>{
+      const db = await open({
+        filename: './src/infra/bd.db',
+        driver: sqlite3.Database
+      })
+      const result = await db.all('SELECT * FROM usuario')
+      res.send(result)
+      db.close()
+    })()
+  }
+
+}
+
+export default UsuarioController
