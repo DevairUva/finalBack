1e499456533ee8c98e472014944af61ef31fbecb
DEVair Martins <devair.martins@gmail.com>
2023-03-24 14:06:38 -0300

    ta indo

---
+++ src/models/Polos.js
@@ -0,0 +1,12 @@
+class Polos{
+  constructor(nome, local, coordenador, historia, professor, qtd_alunos){
+    this.nome = nome;
+    this.local = local;
+    this.coordenador = coordenador;
+    this.historia = historia;
+    this.professor = professor;
+    this.qtd_alunos = qtd_alunos;
+  }
+}
+
+export default Polos;
---
+++ src/models/Usuarios.js
@@ -0,0 +1,10 @@
+class Usuarios {
+  constructor(nome, email, senha, foto) {
+    this.nome = nome;
+    this.email = email;
+    this.senha = senha;
+    this.foto = foto;
+  }
+}
+
+export default Usuarios;
