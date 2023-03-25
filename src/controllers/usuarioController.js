863cca02eb4221fcdfeba5fbff74ba3156d8df4d
DEVair Martins <devair.martins@gmail.com>
2023-03-25 17:18:23 -0300

    get polos

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -3,14 +3,27 @@ import { open } from 'sqlite'
 
 function UsuarioController(app){
 
-  app.get('/usuario', exibir)
-  function exibir (req, res){
+  app.get('/usuario', exibirUsuarios)
+  function exibirUsuarios (req, res){
     (async () =>{
       const db = await open({
         filename: './src/infra/bd.db',
         driver: sqlite3.Database
       })
       const result = await db.all('SELECT * FROM usuarios')
+      res.send(result)
+      db.close()
+    })()
+  }
+
+  app.get('/polo', exibirPolos)
+  function exibirPolos (req, res){
+    (async () =>{
+      const db = await open({
+        filename: './src/infra/bd.db',
+        driver: sqlite3.Database
+      })
+      const result = await db.all('SELECT * FROM polos')
       res.send(result)
       db.close()
     })()
