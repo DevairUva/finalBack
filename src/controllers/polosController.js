dc5584712013cee2e985c8dab469f820b294d0c6
DEVair Martins <devair.martins@gmail.com>
2023-03-29 13:29:37 -0300

    selecionar polos

--- src/controllers/polosController.js
+++ src/controllers/polosController.js
@@ -1,3 +1,6 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
 function PolosController(app) {
 
   app.get('/polo', exibirPolos)
@@ -7,14 +10,14 @@ function PolosController(app) {
         filename: './src/infra/bd.db',
         driver: sqlite3.Database
       })
-      const result = await db.all('SELECT * FROM polos')
+      const result = await db.all('SELECT * FROM usuarios')
       res.send(result)
       db.close()
     })()
   }
 
-  app.get('/polo/:polo', exibirPolos)
-  function exibirPolos(req, res) {
+  app.get('/polo/:polo', selecionarPolos)
+  function selecionarPolos(req, res) {
     (async () => {
       const db = await open({
         filename: './src/infra/bd.db',
